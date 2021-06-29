ed5983ed58fd873ec6000cbba09bef01ac5dfb9f
Thom Williams <thomwilliams1990@gmail.com>
2021-06-29 20:49:38 +0100

    Models: Keys Updated

--- models/Workouts.js
+++ models/Workouts.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutsSchema = new Schema({
-    date: Date,
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
+  day: Date,
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workouts", WorkoutsSchema);
